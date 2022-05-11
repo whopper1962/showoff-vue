4cda1a63b3db986d1e10dc6325afc6c5d73625a0
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-05-11 12:17:42 +0900

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Example</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script defer src="{{ mix('js/app.js') }}"></script> 
+</head>
+<body>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</body>
+</html>
